5f8e463551bedfcbf6e43763a4c44cac0ba97f18
billyovo <30436689+billyovo@users.noreply.github.com>
2023-11-04 16:53:29 +0800

    change status

--- src/utils/discord/updateStatus.ts
+++ src/utils/discord/updateStatus.ts
@@ -4,7 +4,8 @@ import { ExtendedDiscordClient } from "../../managers/discord/client";
 
 export function updateStatus(client : ExtendedDiscordClient, eventsToday: Collection<string, EventScheduleItem>) {
 	if (eventsToday.size > 0) {
-		client.user?.setActivity(`今天的${eventsToday.size}個小遊戲`, { type: ActivityType.Playing });
+		const eventNames : string = eventsToday.map((event : EventScheduleItem) => event.title).join(" ");
+		client.user?.setActivity(eventNames, { type: ActivityType.Playing });
 	}
 	else {
 		client.user?.setActivity("今天沒有小遊戲 :(", { type: ActivityType.Watching });
