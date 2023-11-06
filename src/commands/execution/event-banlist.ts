bf348bcb6647833abd4c3c8bbfcd3c05f2d87556
billyovo <30436689+billyovo@users.noreply.github.com>
2023-11-06 11:55:10 +0800

    fix banlist

--- src/commands/execution/event-banlist.ts
+++ src/commands/execution/event-banlist.ts
@@ -4,7 +4,6 @@ import { getEventBanlist } from "../../utils/database";
 import { embedColor } from "../../constants/embeds";
 import { EmbedBuilder } from "discord.js";
 import { WinnerRecord } from "../../@types/database";
-import { eventSchedule } from "../../managers/eventScheduleManager";
 export async function execute(interaction: ChatInputCommandInteraction) {
 	const server = interaction.options.getString("server");
 	const serverChineseName = ServerNameChineseEnum[server as keyof typeof ServerNameChineseEnum];
@@ -15,7 +14,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 		.setColor(embedColor)
 		.setTitle(`${serverChineseName}服 禁賽名單`);
 	banlist.forEach((record : WinnerRecord) => {
-		embed.addFields({ name:`${eventSchedule.list.get(record.event)?.emote} ${record.event}`, value: record.name, inline: true });
+		embed.addFields({ name:`${record.event}`, value: record.name, inline: true });
 	});
 	interaction.reply({
 		embeds: [embed],
