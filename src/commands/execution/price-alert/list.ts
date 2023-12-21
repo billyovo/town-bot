0fd1298a56228a45b1ea867742211d8287575029
billyovo <30436689+billyovo@users.noreply.github.com>
2023-12-21 13:35:43 +0800

    Refactor price alert list command to handle long lists

--- src/commands/execution/price-alert/list.ts
+++ src/commands/execution/price-alert/list.ts
@@ -4,9 +4,18 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 	const collection = db.collection("products");
 	const products = await collection.find({}).toArray();
 
-	let list = "";
+	const list : string[] = [""];
+	let current = 0;
 	for (let i = 0; i < products.length; i++) {
-		list += `${i + 1}. ${products[i].brand} [${products[i].productName}](<${products[i].url}>)\n`;
+		const message = `${i + 1}. ${products[i].brand} [${products[i].productName}](<${products[i].url}>)\n`;
+		if ((list[current].length + message.length) > 2000) {
+			current++;
+			list[current] = "";
+		}
+		list[current] += message;
 	}
-	await interaction.reply({ content: list });
+	await interaction.reply({ content: list[0] });
+	for (let i = 1; i < list.length; i++) {
+		await interaction.followUp({ content: list[i] });
+	}
 }
