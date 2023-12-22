eb8c33d4dab7877c3e45d126197a10d3615b9116
billyovo <30436689+billyovo@users.noreply.github.com>
2023-12-22 15:03:58 +0800

    Add PNS as a PriceAlertShopOptionImage

--- src/enums/priceAlertShopOption.ts
+++ src/enums/priceAlertShopOption.ts
@@ -9,4 +9,5 @@ export enum PriceAlertShopOptionImage {
     HKTVMALL = "https://i.imgur.com/wYnaMoS.png",
     WATSONS = "https://i.imgur.com/VJ2JDG9.png",
     AEONCITY = "https://i.imgur.com/nopW4EX.png",
+    PNS = "https://i.imgur.com/cT0UG1S.png",
 }
