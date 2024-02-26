01304fd1e26a56d3d3dac9c212968881f8187d2f
billyovo <30436689+billyovo@users.noreply.github.com>
2024-02-26 19:22:22 +0800

    Fix supported shops display in price alert execution

--- src/commands/execution/price-alert/supported.ts
+++ src/commands/execution/price-alert/supported.ts
@@ -2,7 +2,7 @@ import { ChatInputCommandInteraction } from "discord.js";
 import { PriceAlertShopOption } from "~/enums/priceAlertShopOption";
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-	const supportedShops = Object.values(PriceAlertShopOption).join(", ");
+	const supportedShops = Object.values(PriceAlertShopOption);
 
-	interaction.reply({ content: `${supportedShops.length} supported shops: ${supportedShops}` });
+	interaction.reply({ content: `${supportedShops.length} supported shops: ${supportedShops.join(", ")}` });
 }
