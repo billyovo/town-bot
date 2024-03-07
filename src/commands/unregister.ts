2f1a3a7fc45a3b0565bda9952da9bbc06fa577fe
billyovo <30436689+billyovo@users.noreply.github.com>
2024-03-07 17:32:39 +0800

    [ci skip]: add unregister commands

---
+++ src/commands/unregister.ts
@@ -0,0 +1,22 @@
+import { Routes, REST } from "discord.js";
+import path from "path";
+import dotenv from "dotenv";
+import { logger } from "../logger/logger";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+if (!process.env.CLIENT_ID || !process.env.DISCORD_TOKEN) {
+	console.error("Failed to register command: Missing CLIENT_ID or DISCORD_TOKEN in .env file");
+	process.exit(1);
+}
+
+const rest = new REST().setToken(`${process.env.DISCORD_TOKEN}`);
+
+(async () => {
+	const data = await rest.put(
+		Routes.applicationCommands(`${process.env.CLIENT_ID}`),
+		{ body: [] },
+	);
+	logger(JSON.stringify(data));
+})();
+
