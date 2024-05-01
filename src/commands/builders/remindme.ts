edc0215d184aa3f424d3c84bb41ce5024259c8db
billyovo <30436689+billyovo@users.noreply.github.com>
2024-05-01 21:32:48 +0800

    chore: update remindme description

--- src/commands/builders/remindme.ts
+++ src/commands/builders/remindme.ts
@@ -3,6 +3,6 @@ import { SlashCommandBuilder } from "discord.js";
 export const command = new SlashCommandBuilder()
 	.setName("remindme")
 	.setDescription("remind me")
-	.addStringOption(option => option.setName("time").setDescription("time").setRequired(true))
+	.addStringOption(option => option.setName("time").setDescription("duration string like 6h30m, 1y3M, 5mins12s").setRequired(true))
 	.addStringOption(option => option.setName("message").setDescription("message").setRequired(true))
 	.addBooleanOption(option => option.setName("dm").setDescription("should this reminder be a DM?").setRequired(false));
