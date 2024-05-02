2e0ffb1edb15bdb12872b8c86984d857865df02c
alexchoicy <alex123321choicy@gmail.com>
2024-05-02 20:27:27 +0800

    reschedule the time of hko reminder to match the correct date

--- src/cronJobs/jobs.ts
+++ src/cronJobs/jobs.ts
@@ -7,35 +7,49 @@ import { DateTime } from "luxon";
 import { scheduleJob } from "node-schedule";
 import { getCatFact } from "~/utils/catFact";
 import { logger } from "~/logger/logger";
+import { ReminderModel } from "~/database/schemas/reminders";
 
 scheduleJob("1 0 * * *", () => {
 	const now = DateTime.now().setZone("Asia/Taipei").endOf("day");
 
 	client.channels.fetch(process.env.HAPPY_CHANNEL as string, { force: true, cache: false })
-		.then((channel : Channel | null) => {
+		.then((channel: Channel | null) => {
 			if (channel?.type !== ChannelType.GuildText) return;
 
 			channel.send(`@everyone 小妹有錢人生活 ${returnDiff(now)} **POSITIVE**`);
 		})
-		.catch((error) => {logger(error.message);});
+		.catch((error) => { logger(error.message); });
 
 });
 
 scheduleJob("0 0 * * *", async () => {
 	const catFact = await getCatFact();
-	client.user!.setActivity({ name: catFact, type: ActivityType.Custom });
+  client.user!.setActivity({ name: catFact, type: ActivityType.Custom });
 });
 
-scheduleJob("0 7 * * *", async () => {
+scheduleJob("0 6 * * *", async () => {
 	try {
 		const respone = await callhko();
 		const rain = respone.data.weatherForecast[0].PSR;
 		if (rain === HkoRainOption.HIGH || rain === HkoRainOption.MEDIUM_HIGH) {
-			client.channels.fetch((process.env.WEATHER_CHANNEL) as string, { force: true, cache: false })
-				.then((channel : Channel | null) => {
-					(channel as TextChannel).send("出門口記得帶遮!!!!!!!!!!");
-				})
-				.catch((error) => {logger(error.message);});
+			const reminderTime = DateTime.now().plus({ days: 1 });
+			const message = "出門口記得帶遮!!!!!!!!!!";
+			const reminder = await ReminderModel.create({
+				sendTime: reminderTime,
+				message: message,
+				isDm: false,
+				owner: process.env.CLIENT_ID,
+				channel: process.env.WEATHER_CHANNEL,
+			});
+
+			scheduleJob(reminderTime.toJSDate(), () => {
+				client.channels.fetch((process.env.WEATHER_CHANNEL) as string, { force: true, cache: false })
+					.then((channel: Channel | null) => {
+						(channel as TextChannel).send(message);
+					})
+					.catch((error) => { logger(error.message); });
+				ReminderModel.deleteOne({ _id: reminder.id }).exec();
+			});
 		}
 	}
 	catch (error) {
