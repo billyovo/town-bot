775d5054fb1982695722fefcdac0081b7d4f0a7e
billyovo <30436689+billyovo@users.noreply.github.com>
2024-05-29 14:01:40 +0800

    refactor: improve autocomplete in price alert add command

--- src/discord/commands/autoComplete/price-alert/add.ts
+++ src/discord/commands/autoComplete/price-alert/add.ts
@@ -8,27 +8,54 @@ export async function autoComplete(interaction : AutocompleteInteraction) {
 	const brand = interaction.options.getString("brand") ?? "";
 	const shop = interaction.options.getString("shop") ?? "";
 
-	const productNameQuery = productName ? { $regex: productName, $options: "i" } : { $exists: true };
-	const brandQuery = brand ? { $regex: brand, $options: "i" } : { $exists: true };
-	const shopQuery = shop ? { $regex: shop, $options: "i" } : { $exists: true };
+	const productNameQuery = productName ? { productName: productName } : {};
+	const brandQuery = brand ? { brand: brand } : { };
+	const shopQuery = shop ? { shop: shop } : { };
 
 	const groupQuery : {
         [key: string]: string;
     } = {};
 
 	// whichever option is focused, we want to use that as the key in the group query
-	groupQuery[focusedOption.name] = `$${focusedOption.name}`;
+	if (focusedOption.name === "name") {
+		groupQuery.productName = "$productName";
+	}
+
+	if (focusedOption.name === "brand") {
+		groupQuery.brand = "$brand";
+	}
+
+	if (focusedOption.name === "shop") {
+		groupQuery.shop = "$shop";
+	}
 
 	const products = await PriceAlertModel.aggregate([
-		{ $match: { productName: productNameQuery, brand: brandQuery, shop: shopQuery } },
+		{ $match: { ...productNameQuery, ...brandQuery, ...shopQuery } },
 		{ $group: { _id: groupQuery } },
 		{ $limit: 15 },
 	]).exec();
 
-	return await interaction.respond(
-		products.map(product => ({
-			name: product._id[focusedOption.name],
-			value: product._id[focusedOption.name],
-		}),
-		));
+	if (focusedOption.name === "name") {
+		return interaction.respond(products.map(product => ({
+			name: product._id.productName,
+			value: product._id.productName,
+		})));
+	}
+
+	if (focusedOption.name === "brand") {
+		return interaction.respond(products.map(product => ({
+			name: product._id.brand,
+			value: product._id.brand,
+		})),
+		);
+	}
+
+	if (focusedOption.name === "shop") {
+		return interaction.respond(products.map(product => ({
+			name: product._id.shop,
+			value: product._id.shop,
+		})),
+		);
+	}
+
 }
