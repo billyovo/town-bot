ed531c2b7beaff04f29e8c9933aef286c7a3a776
billyovo <30436689+billyovo@users.noreply.github.com>
2024-07-19 12:48:40 +0800

    update shop emoji to app-owned emoji

--- src/lib/price-alert/utils/enums/priceAlertShopOption.ts
+++ src/lib/price-alert/utils/enums/priceAlertShopOption.ts
@@ -30,47 +30,47 @@ export const PriceAlertShopParseDetails: Record<PriceAlertShopOption, ParseDetai
 	[PriceAlertShopOption.HKTVMALL]: {
 		image: "https://i.imgur.com/wYnaMoS.png",
 		parseFunction: parseHktvmallPrice,
-		emote: "<:hktvmall:1245282184752009277>",
+		emote: "<:hktvmall:1263718464519143486>",
 	},
 	[PriceAlertShopOption.WATSONS]: {
 		image: "https://i.imgur.com/VJ2JDG9.png",
 		parseFunction: parseWatsonsGroupPrice,
-		emote: "<:watsons:1245282256348516392>",
+		emote: "<:watsons:1263718482592268368>",
 	},
 	[PriceAlertShopOption.AEONCITY]: {
 		image: "https://i.imgur.com/nopW4EX.png",
 		parseFunction: parseAeonPrice,
-		emote: "<:aeon:1245282318390919198>",
+		emote: "<:aeon:1263718492322922527>",
 	},
 	[PriceAlertShopOption.PNS]: {
 		image: "https://i.imgur.com/cT0UG1S.png",
 		parseFunction: parseWatsonsGroupPrice,
-		emote: "<:PNS:1245282382857244715>",
+		emote: "<:PNS:1263718500950868019>",
 	},
 	[PriceAlertShopOption.SEPHORA]: {
 		image: "https://pbs.twimg.com/profile_images/459373077822861312/nHUto8C6_400x400.jpeg",
 		parseFunction: parseSephoraPrice,
-		emote: "<:sephora:1245282556362887218>",
+		emote: "<:sephora:1263718515488325734>",
 	},
 	[PriceAlertShopOption.MANNINGS]: {
 		image: "https://i.imgur.com/OnplWyK.jpg",
 		parseFunction: parseManningsPrice,
-		emote: "<:mannings:1245282558477078528>",
+		emote: "<:mannings:1263718526729060383>",
 	},
 	[PriceAlertShopOption.MUJI]: {
 		image: "https://i.imgur.com/ykFhg6J.png",
 		parseFunction: parseMujiPrice,
-		emote: "<:muji:1245282560829820958>",
+		emote: "<:muji:1263718540322803804>",
 	},
 	[PriceAlertShopOption.WELLCOME]: {
 		image: "https://i.imgur.com/A5ogcsY.png",
 		parseFunction: parseWellcomePrice,
-		emote: "<:wellcome:1245282562717257759>",
+		emote: "<:wellcome:1263718551424991243>",
 	},
 	[PriceAlertShopOption.LOOKFANTASTIC]: {
 		image: "https://pbs.twimg.com/profile_images/1687076928635711488/G0CE0Q8-_400x400.jpg",
 		parseFunction: parseLookfantasticPrice,
-		emote: "<:lookfantastic:1245282564902621226>",
+		emote: "<:lookfantastic:1263718561529069578>",
 	},
 } satisfies Record<PriceAlertShopOption, ParseDetails>;
 
