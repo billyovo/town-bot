f471fb6c30e4c32b3da41d3ff0c9cd91d0cf5109
billyovo <30436689+billyovo@users.noreply.github.com>
2024-07-20 18:14:48 +0800

    chore: Fix error handling in anya-lovethis.ts

--- src/discord/commands/execution/anya-lovethis.ts
+++ src/discord/commands/execution/anya-lovethis.ts
@@ -9,20 +9,25 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 		interaction.reply({ content: "Nothing is received!", ephemeral: true });
 		return;
 	}
-	await interaction.deferReply();
 	const canvas = createCanvas(1000, 560);
 	const ctx = canvas.getContext("2d");
 
 	try {
 		const base = await loadImage(path.resolve(__dirname, "../../../assets/lovethis.png"));
 		const image = await loadImage(url);
+		await interaction.deferReply();
 		ctx.drawImage(image, 0, 0, image.width, image.height, 313, 79, 339, 263);
 		ctx.drawImage(base, 0, 0);
 		const attachment = new AttachmentBuilder(canvas.toBuffer("image/png"), { name: "output.png" });
 		interaction.editReply({ files: [attachment] });
 	}
 	catch (error) {
-		interaction.editReply({ content: "an error occured!" });
+		if (interaction.replied) {
+			interaction.editReply({ content: "an error occured!" });
+		}
+		else {
+			interaction.reply({ content: "an error occured!", ephemeral: true });
+		}
 		if (error instanceof Error) {
 			log(error.message);
 		}
