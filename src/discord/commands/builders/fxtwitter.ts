be763c6b653ea16242cf687132cccaf418a8b3cf
billyovo <30436689+billyovo@users.noreply.github.com>
2024-09-03 13:08:26 +0800

    fix integration type for fxtwitter

--- src/discord/commands/builders/fxtwitter.ts
+++ src/discord/commands/builders/fxtwitter.ts
@@ -1,6 +1,7 @@
-import { SlashCommandBuilder } from "discord.js";
+import { ApplicationIntegrationType, SlashCommandBuilder } from "discord.js";
 
 export const command = new SlashCommandBuilder()
 	.setName("fxtwitter")
 	.setDescription("fix twitter")
-	.addStringOption(option => option.setName("link").setDescription("link").setRequired(true));
+	.addStringOption(option => option.setName("link").setDescription("link").setRequired(true))
+	.setIntegrationTypes(ApplicationIntegrationType.UserInstall);
