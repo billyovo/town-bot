c0bf1e6e4d9ee629eb5fd3216377f42462266bf6
alexchoicy <alex123321choicy@gmail.com>
2025-06-16 02:58:36 +0800

    fix mannings

--- src/lib/price-alert/scrape/websites/mannings.ts
+++ src/lib/price-alert/scrape/websites/mannings.ts
@@ -1,136 +1,222 @@
-import { ShopParseFunction } from "~/src/@types/price-alert";
 import { PromotionType } from "~/src/@types/enum/price-alert";
-import { PriceAlertShopOption } from "~/src/lib/price-alert/utils/enums/priceAlertShopOption";
-import { formatBrandName, parsePriceToFloat } from "~/src/lib/price-alert/utils/format";
-import { getHTML } from "../../utils/scrapeGetters";
+import { PromotionClassified, ShopParseFunction } from "~/src/@types/price-alert";
+import { Failure, Success } from "~/src/@types/utils";
 import { logger } from "~/src/lib/logger/logger";
-import { HTMLElement } from "node-html-parser";
-import { classifyPromotionByKeywords } from "../../utils/classifyPromotions";
-
-export const parseManningsPrice: ShopParseFunction = async (url, _, __) => {
-	const html = await getHTML(url);
-	if (!html.success) return { success: false, error: html.error, data: null };
-
-	const root = html.data;
-
-	const allScripts : HTMLElement[] | null = root.querySelectorAll("script[type=\"text/javascript\"]");
-	if (!allScripts) return { success: false, error: "Scripts not found", data: null };
-
-	const productDataScript = getProductScriptFromScripts(allScripts);
-	if (!productDataScript) return { success: false, error: "Product data not found", data: null };
-
-	const parsedProductData = getProductDataFromScript(productDataScript);
-
-	if (!parsedProductData) return { success: false, error: "Product data parsing failed", data: null };
-
-	const productName = parsedProductData?.name;
-	const price = (parsedProductData?.price && parsedProductData?.metric1) ? (parsePriceToFloat(parsedProductData.price) - parsePriceToFloat(parsedProductData.metric1)) : null;
-	const brand = parsedProductData?.brand;
-	const productImage = root.querySelector("meta[property=\"og:image\"]")?.getAttribute("content");
-
-	if (!price) return { success: false, error: "Price not found", data: null };
-	if (!productName) return { success: false, error: "Product name not found", data: null };
+import { APIClient } from "~/src/lib/utils/fetch/client";
+import { PriceAlertShopOption } from "../../utils/enums/priceAlertShopOption";
 
 
-	const promotions = root.querySelectorAll(".promotion-text.promotion_description_holder").map((promotion) => {
-		return promotion.text.trim();
-	});
+export const parseManningsPrice: ShopParseFunction = async (url) => {
+	const match = url.match(/\/p\/(\d+)(\/|$|\?)/);
 
-	const classifiedPromotions = classifyPromotions(promotions);
+	if (!match || match.length < 2) {
+		return {
+			data: null,
+			error: "Invalid URL format",
+			success: false,
+		};
+	}
+
+
+	const skuID = match[1];
+
+	const productDetails = await fetchProductDetail(skuID);
+	if (!productDetails.success) {
+		return {
+			data: null,
+			error: productDetails.error ?? "Failed to fetch product details",
+			success: false,
+		};
+	}
+
+	const productImage = getProductImage(productDetails.data.media_gallery_entries);
+	const promotions = getPromotions(productDetails.data.offers);
+
+	const data = {
+		brand: productDetails.data.brand_information.name,
+		productName: productDetails.data.name,
+		productImage: productImage,
+		price: productDetails.data.price_range.maximum_price.final_price.value,
+		shop: PriceAlertShopOption.MANNINGS,
+		promotions: promotions,
+	};
 
 	return {
-		data: {
-			price: price,
-			productName: productName,
-			productImage: productImage ?? "",
-			brand: formatBrandName(brand) || "No Brand",
-			shop: PriceAlertShopOption.MANNINGS,
-			promotions: classifiedPromotions,
-		},
+		data,
 		error: null,
 		success: true,
 	};
-
 };
 
-function getProductScriptFromScripts(allScripts: HTMLElement[]) {
-	for (let i = 0; i < allScripts.length; i++) {
-		const script = allScripts[i];
-		if (script.textContent.includes("function trackProductDetail()")) {
-			return script.text;
+function getPromotions(offers: Offer[]): PromotionClassified[] | null {
+	const parsedPromotions = offers.map((offer) => {
+		const parsePromotMessage = getPromoteMessage(offer);
+
+		if (offer.free_gift.is_freegift) {
+			if (offer.free_gift.available) {
+				return {
+					type: PromotionType.FREEBIE,
+					description: parsePromotMessage,
+				};
+			}
+			// If the free gift is not available, we do not return it as a promotion
+			else {
+				return null;
+			}
 		}
+
+		return {
+			type: PromotionType.DISCOUNT,
+			description: parsePromotMessage,
+		};
+	}).filter((p): p is PromotionClassified => p !== null);
+
+	if (parsedPromotions.length === 0) {
+		return null;
 	}
 
+	return parsedPromotions;
+
+}
+function getPromoteMessage(offer: Offer): string {
+	const message = offer.content.map((c) => {
+		if (c.message === c.description) {
+			return c.message;
+		}
+		return `${c.message} - ${c.description}`;
+	});
+
+	if (message.length === 0) {
+		return "No promotion message available";
+	}
+
+	return message.join(", ");
+}
+
+function getProductImage(media: MediaEntry[]): string | null {
+	const BASEIMAGEURL = "https://www.mannings.com.hk/media/catalog/product";
+
+	if (media.length === 0) {
+		return null;
+	}
+
+	media.sort((a, b) => a.position - b.position);
+
+	for (const entry of media) {
+		if (entry.media_type === "image" && !entry.disabled) {
+			return `${BASEIMAGEURL}/${entry.file}`;
+		}
+	}
 	return null;
 }
 
-function getProductDataFromScript(script: string) {
-	/*
-	   say we have a <script> tag with variables like
-	   function trackProductDetail() {
-	    //console.log('trackProductDetail');
-        dataLayer.push({
-          'event': 'productDetail',
-          'ecommerce': {
-            'detail': {
-              'products': [
 
-                {
-                'name': 'Mannings Apple Cider Vinegar Gummies 60pcs',
-                'id': '752642',
-                'price': '178.0',
-                'metric1': '40.0',
-                'brand':  'Mannings',
-                'variant': '60pcs',
-                'category':'INTAKE'
-                }
-               ]
-             }
-           }
-        });
+async function fetchProductDetail(skuID: string): Promise<Success<ProductItem> | Failure> {
+	const BASEURL = "https://www.mannings.com.hk/graphql";
+	const query = `
+		query getProductDetailForProductPage($sku: String!) {
+		productsV2(filter: { sku: { eq: $sku } }) {
+			items {
+			name
+			sku
+			price_range {
+				maximum_price {
+				final_price {
+					currency
+					value
+				}
+				}
+			}
+			offers {
+				free_gift {
+				is_freegift
+				available
+				}
+				content {
+				message
+				description
+				}
+				is_yuu_promotion
+				__typename
+			}
+			brand_information {
+				name
+			}
+			media_gallery_entries {
+				position
+				disabled
+				file
+				media_type
+			}
+			}
+		}
+		}
+  `;
 
-	   we only know which line the productDetailPageProductData is in, and the data could have nested objects and spam multiple lines
-	   we need to find the start and end of the object, and extract it
-
-		assume it's always an valid json object
-		use the classic stack method to find the start and end of the object, meet "{" stack++, meet "}" stack--, loop until stack = 0
-	*/
-	const productDataIndex = script.indexOf("'products'") + "'products'".length;
-
-	let start : number = 0;
-	let stack : number = 1;
-
-	while (script[productDataIndex + start - 1] !== "{") {
-		start++;
-	}
-
-	let receivedData = "{";
-
-	// don't use falsey here, empty string is falsey
-	while ((stack > 0) && script[productDataIndex + start] !== undefined) {
-		const char = script[productDataIndex + start];
-		if (char === "{") stack++;
-		if (char === "}") stack--;
-		receivedData += char;
-		start++;
-	}
-
-	const sanitizedData = receivedData.replaceAll("'", "\"");
-
-	try {
-		return JSON.parse(sanitizedData);
-	}
-	catch (error) {
-		logger.error((error as Error).message);
-		return null;
-	}
-}
-
-function classifyPromotions(promotions: string[]) {
-	const classified = promotions.map((promotion) => {
-		const type : PromotionType = classifyPromotionByKeywords(promotion);
-		return { type, description: promotion };
+	const params = new URLSearchParams({
+		query,
+		variables: JSON.stringify({ sku: skuID }),
+		operationName: "getProductDetailForProductPage",
 	});
 
-	return classified;
+	let productDetailsRes;
+	const url = `${BASEURL}?${params.toString()}`;
+	try {
+		productDetailsRes = await APIClient.get(url, {
+			headers: {
+				"store": "zh_HK",
+			},
+		});
+	}
+	catch (error) {
+		logger.error(`Cannot Get ${url}: ${error}`);
+		return {
+			data: null,
+			error: "Product not found",
+			success: false,
+		};
+	}
+	return {
+		data: productDetailsRes.data.data.productsV2.items[0],
+		error: null,
+		success: true,
+	};
+}
+
+type ProductItem = {
+	name: string;
+	sku: string;
+	price_range: {
+		maximum_price: {
+			final_price: {
+				currency: string;
+				value: number;
+			};
+		};
+	};
+	offers: Offer[];
+	brand_information: {
+		name: string;
+	};
+	media_gallery_entries: MediaEntry[];
+}
+
+type Offer = {
+	free_gift: {
+		is_freegift: boolean;
+		available: boolean;
+	};
+	content: {
+		message: string;
+		description: string;
+	}[];
+	is_yuu_promotion: boolean;
+	__typename: string;
+}
+
+type MediaEntry = {
+	position: number;
+	disabled: boolean;
+	file: string;
+	media_type: string;
 }
