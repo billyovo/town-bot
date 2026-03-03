9b1ee91406aba812651ecfc38820b309adf8ad76
billyovo <30436689+billyovo@users.noreply.github.com>
2026-03-03 21:26:34 +0800

    feat: banana!!!

--- src/discord/commands/builders/slash/draw.ts
+++ src/discord/commands/builders/slash/draw.ts
@@ -12,36 +12,25 @@ export const command = new SlashCommandBuilder()
 			.setDescription("size")
 			.setRequired(false)
 			.addChoices(
-				{
-					"name": "1024x1024",
-					"value": "1024x1024",
-				},
-				{
-					"name": "1024x1536",
-					"value": "1024x1536",
-				},
-				{
-					"name": "1536x1024",
-					"value": "1536x1024",
-				},
+				{ "name": "1:1", "value": "1024x1024" },
+				{ "name": "16:9", "value": "1344x768" },
+				{ "name": "4:3", "value": "1184x864" },
+				{ "name": "9:16", "value": "768x1344" },
+				{ "name": "21:9", "value": "1536x672" },
+				{ "name": "2:3", "value": "832x1248" },
+				{ "name": "3:2", "value": "1248x832" },
+				{ "name": "3:4", "value": "864x1184" },
+				{ "name": "4:5", "value": "896x1152" },
+				{ "name": "5:4", "value": "1152x896" },
 			),
 	)
 	.addStringOption(option =>
-		option.setName("quality")
-			.setDescription("quality")
-			.setRequired(false)
-			.addChoices(
-				{
-					"name": "low",
-					"value": "low",
-				},
-				{
-					"name": "medium",
-					"value": "medium",
-				},
-				{
-					"name": "high",
-					"value": "high",
-				},
-			),
+		option.setName("image_url")
+			.setDescription("image url")
+			.setRequired(false),
+	)
+	.addAttachmentOption(option =>
+		option.setName("image")
+			.setDescription("image")
+			.setRequired(false),
 	);
--- src/discord/commands/execution/draw.ts
+++ src/discord/commands/execution/draw.ts
@@ -2,63 +2,79 @@ import { ChatInputCommandInteraction } from "discord.js";
 import axios from "axios";
 import { logger } from "~/src/lib/logger/logger";
 
+function createPromptArray(prompt: string, imageBase64: string | null) {
+	const builtPrompt = [];
+	if (imageBase64) {
+		builtPrompt.push({
+			role: "user",
+			parts: [{
+				inline_data: {
+					mime_type: "image/png",
+					data: imageBase64,
+				},
+			}],
+		});
+	}
+	builtPrompt.push({
+		role: "user",
+		parts: [{
+			text: prompt,
+		}],
+	});
+	return builtPrompt;
+}
+
 export async function execute(interaction: ChatInputCommandInteraction) {
 	if (!process.env.DRAW_LINK || !process.env.DRAW_KEY || !process.env.DRAW_API_VERSION) {
 		await interaction.reply("The draw API token is not provided.");
 		return;
 	}
+	const prompt = interaction.options.getString("prompt");
+	if (!prompt) {
+		await interaction.reply({ content: "Prompt is required.", ephemeral: true });
+		return;
+	}
+	const aspectRatio = interaction.options.getString("size") || "1:1";
 
-	const prompt : string = interaction.options!.getString("prompt")?.trim() ?? "";
-	const size : string = interaction.options.getString("size") ?? "1024x1024";
-	const quality : string = interaction.options.getString("quality") ?? "high";
+	const imageURL = interaction.options.getString("image_url");
+	const image = interaction.options.getAttachment("image")?.url;
 
-	const headers = {
-		"Cache-Control": "no-cache, must-revalidate",
-		"Content-Type": "application/json",
-		"api-key": process.env.DRAW_KEY,
-	};
-	const body = {
-		prompt: prompt,
-		size: size,
-		n: 1,
-		quality: quality,
-		model: "gpt-image-1",
-	};
+	const finalImageURL : string | undefined = imageURL || image;
+	const imageBase64 : string | null = finalImageURL ? await axios.get(finalImageURL, { responseType: "arraybuffer" }).then(response => Buffer.from(response.data, "binary").toString("base64")) : null;
+
+	if (finalImageURL && !imageBase64) {
+		logger.error(`Failed to fetch image from URL: ${finalImageURL}`);
+		await interaction.reply({ content: "Failed to fetch the provided image", ephemeral: true });
+		return;
+	}
 
 	await interaction.deferReply();
+	const res = await axios.post(process.env.DRAW_LINK, {
+		contents: createPromptArray(prompt, imageBase64),
+		generationConfig: {
+			responseModalities: ["Image"],
+			imageConfig: {
+				aspectRatio: aspectRatio,
+			},
+		},
+	}, {
+		headers: {
+			"api-key": process.env.DRAW_KEY,
+		},
+	}).catch(async err => {
+		logger.error(`Error while calling image generation ${JSON.stringify(err?.response?.data) || err.message}`);
+		await interaction.editReply({ content: "Error occured while calling image generation API" });
+		return null;
+	});
 
-	let imageRequest;
-	try {
-		imageRequest = await axios.post(process.env.DRAW_LINK, body, {
-			headers,
-			params: { "api-version": process.env.DRAW_API_VERSION },
-			timeout: 120000,
-		});
-	}
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	catch (error: any) {
-		const msg =
-			error?.response?.data?.error?.message ??
-			error?.response?.data?.message ??
-			error?.message ??
-			"Failed to generate image";
-		logger.error(msg);
-		await interaction.editReply(msg);
+	if (!res) return;
+
+	const responsedImageBase64 = res?.data?.candidates?.[0]?.content?.parts?.[0]?.inlineData?.data;
+	if (!responsedImageBase64) {
+		logger.error("No image data received from the API");
+		await interaction.editReply({ content: "No image data received from the API" });
 		return;
 	}
-
-	if (!imageRequest) {
-		await interaction.editReply("An error occurred when sending the request.");
-		return;
-	}
-
-	const imageBase64 : string = imageRequest.data.data?.[0]?.b64_json;
-
-	if (!imageBase64) {
-		await interaction.editReply({ content: "Image not found in response." });
-		return;
-	}
-
-	const imageBuffer : Buffer = Buffer.from(imageBase64, "base64");
+	const imageBuffer : Buffer = Buffer.from(responsedImageBase64, "base64");
 	await interaction.editReply({ files: [imageBuffer], content: prompt });
 }
